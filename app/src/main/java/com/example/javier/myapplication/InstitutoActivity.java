1aa1b69c5de9d2ceccbf1bda50776319e8091358
javiercrer <javiercrer@gmail.com>
2018-11-17 08:37:17 -0300

    First Commit

---
+++ app/src/main/java/com/example/javier/myapplication/InstitutoActivity.java
@@ -0,0 +1,32 @@
+package com.example.javier.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class InstitutoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_instituto);
+
+        /* adapt the image to the size of the display */
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        Bitmap bmp = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(),R.drawable.fondo),size.x,size.y,true);
+        ImageView iv_background = (ImageView) findViewById(R.id.imageView3);
+        iv_background.setImageBitmap(bmp);
+    }
+}
